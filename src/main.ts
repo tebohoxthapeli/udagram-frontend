1366a762b26427a18725d2b3dd5f10f3538d3f24
Teboho Thapeli <tm.thapeli@gmail.com>
2022-07-12 21:46:46 +0200

    ---

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,6 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { AppModule } from "./app/app.module";
 import { environment } from "./environments/environment";
 
-try {
-	console.log("frontend port:", process.env.PORT);
-} catch (e) {
-	console.log("failed to get PORT");
-}
-
 if (environment.production) {
 	enableProdMode();
 }
